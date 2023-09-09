7ef886e85dc3e3876a0b45f36b0b2c35b3a6d650
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-09 05:51:02 -0700

    setup database connection

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
+require("dotenv").config();
+
+// imports
 const express = require("express");
+const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
+
+// environment variables
+MONGODB_URL = process.env.MONGODB_URL;
+PORT = process.env.PORT;
+
 const app = express();
 
-// start the server
-app.listen(5000, (req, res) => {
-  console.log("server is listening at port 5000");
-});
+// middlewares
+app.use(express.json());
 
-// auth routes
+// create database
+mongoose
+  .connect(MONGODB_URL)
+  .then((result) => {
+    // start the server
+    app.listen(PORT, (req, res) => {
+      console.log("database is connected");
+      console.log("server is listening at port 5000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// routes
 app.use(authRoutes);
