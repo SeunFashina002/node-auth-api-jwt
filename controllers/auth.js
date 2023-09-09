ef6adcdd5e7a5338b19f972ba38ba06acd074ce5
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-09 07:28:38 -0700

    added signup functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,13 @@
 const User = require("../models/user");
 
 const signup = async (req, res) => {
-  res.send("sign up");
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json({ success: true, data: user });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
 };
 
 const login = async (req, res) => {
