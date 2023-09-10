34c5293977e39c309c2f2e2c1c242d6d52a34b36
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-10 13:07:17 -0700

    added logout route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signup, login } = require("../controllers/auth");
+const { signup, login, logout } = require("../controllers/auth");
 
 const router = express.Router();
 
 // set up routes
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/logout", logout);
 
 module.exports = router;
